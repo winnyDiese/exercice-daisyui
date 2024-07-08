b8e557a3c8d6f502c91712445a623c4e43640122
winnyDiese <winnydiese@gmail.com>
2024-07-09 01:09:37 +0200

    insert footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
 
+        {/* Navbar */}
         <div className="navbar bg-base-100">
           <div className="flex-1">
             <a className="btn btn-ghost text-xl">daisyUI</a>
           </div>
+
           <div className="flex-none">
             <div className="dropdown dropdown-end">
               <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
@@ -78,6 +80,30 @@ export default function RootLayout({
         </div>
 
         {children}
+
+        {/* Footer */}
+        <footer className="footer bg-neutral text-neutral-content p-10">
+          <nav>
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Company</h6>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Legal</h6>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </footer>
 
       </body>
     </html>
